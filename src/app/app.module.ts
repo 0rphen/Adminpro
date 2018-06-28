25c631387c98ab3fde92d01b59099d2e2170803c
0rphen <the.bebop.88@gmail.com>
2018-06-27 23:28:05 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTES } from './app.routes';
+APP_ROUTES
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        NopagefoundComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graphics1Component,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+        PagesComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        APP_ROUTES
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
